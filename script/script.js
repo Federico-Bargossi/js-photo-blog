e704560fe9cfa2a2dd7f0d45daa2baac03dbf34e
Federico-Bargosi <bargossifede98@gmail.com>
2024-11-19 17:46:03 +0100

    feat:esercizio completo

---
+++ script/script.js
@@ -0,0 +1,30 @@
+let cards = {};
+let cardItem = "";
+const rowElem = document.querySelector(".row");
+
+const rendereCard = () => {
+    axios
+        .get(`https://jsonplaceholder.typicode.com/photos?_limit=6`)
+        .then(resp => {
+            cards = resp.data;
+            console.log(cards);
+            cards.forEach((cards) => {
+                const cureCardTitle = cards.title;
+                const cureCardUrl = cards.url;
+                const card = `
+        <div class="col">
+                    <div class="card-container">
+                        <img src="./assets_day1/img/pin.svg" class="svg" alt="">
+                        <img src="${cureCardUrl}" alt="">
+                        <p>${cureCardTitle}</p>
+                    </div>
+                </div>
+        `
+                cardItem += card;
+            })
+            rowElem.innerHTML = cardItem;
+
+        })
+}
+
+rendereCard();
